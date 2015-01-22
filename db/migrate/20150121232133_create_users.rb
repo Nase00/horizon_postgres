ed93483ff558c8e365420ec10f871fbdd5a08ab1
Sean <nase00@gmail.com>
2015-01-21 18:29:30 -0600

    Initial User model.

--- db/migrate/20150121232133_create_users.rb
+++ db/migrate/20150121232133_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :username, :email, null: false, index: true
+      t.string :twitter, :facebook
+      t.string :password_digest
 
       t.timestamps null: false
     end
